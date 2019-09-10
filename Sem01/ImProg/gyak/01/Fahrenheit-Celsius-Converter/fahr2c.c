faf68e39380438b5748316964e3c438d3262c9e5
Parrag Szilárd <szilard.parrag@gmail.com>
2019-09-10 23:16:00 +0200

    Fahrenheit to Celsius converter in C and Python.

---
+++ Sem01/ImProg/gyak/01/Fahrenheit-Celsius-Converter/fahr2c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int fahr;
+
+    for (fahr=-100; fahr<=400; fahr+=25)
+{
+    printf("Fahr=%d, \tCels=%f\n", fahr, 5./9.*(fahr-32));
+
+}
+    return 0;
+}
---
+++ Sem01/ImProg/gyak/01/Fahrenheit-Celsius-Converter/fahr2c.py
@@ -0,0 +1,2 @@
+for fahr in range(-100, 425, 25):
+    print("Fahr =", fahr, "Cels =", 5/9*(fahr-32))
