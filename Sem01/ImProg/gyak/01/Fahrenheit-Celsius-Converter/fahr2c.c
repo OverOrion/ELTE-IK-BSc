508e4f559a2e86f0b4407535ce80a02f3ec4503b
Parrag Szilárd <szilard.parrag@gmail.com>
2019-09-12 17:40:33 +0200

    minor changes
    minor changes for homework (-20;120)

--- Sem01/ImProg/gyak/01/Fahrenheit-Celsius-Converter/fahr2c.c
+++ Sem01/ImProg/gyak/01/Fahrenheit-Celsius-Converter/fahr2c.c
@@ -3,9 +3,9 @@ int main()
 {
     int fahr;
 
-    for (fahr=-100; fahr<=400; fahr+=25)
+    for (fahr=-20; fahr<=120; fahr+=10)
 {
-    printf("Fahr=%d, \tCels=%f\n", fahr, 5./9.*(fahr-32));
+    printf("%d Fahrenheit %f Celsius\n", fahr, 5./9.*(fahr-32));
 
 }
     return 0;
