dcda929aa93a541d1386fc2b820c1211a7a5466e
OverOrion <szilard.parrag@gmail.com>
2019-09-19 15:30:21 +0200

    sync

---
+++ Sem01/ImProg/extra/hello03.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{    
+    printf("Add meg a neved max 25 karakterben");
+    char name[25];
+    scanf("%s", name);
+    printf("%s", name);
+}
---
+++ Sem01/ImProg/extra/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ int main()
+ {
+     printf("Add meg a neved max 25 karakterben");
+     char name[25];
+     scanf("%s", name);
+     printf("%s", name);
+ }
