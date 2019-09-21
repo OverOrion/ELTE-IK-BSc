abb6b4ef10966c2c19ed17e240af43cb3c81b1c7
OverOrion <szilard.parrag@gmail.com>
2019-09-21 15:35:38 +0200

    Extra exercises in C
    Also I found a pretty neat GNU C tutorial which might come handy in the future

--- Sem01/ImProg/extra/canvas/hello03.c
+++ Sem01/ImProg/extra/canvas/hello03.c
@@ -1,1 +1,13 @@
 /*A format specifier-ben add meg, hogy mekkora inputtal dolgozol. */
+
+#include <stdio.h>
+
+int main()
+{
+	char nev[12] ;
+	printf("Add meg a neved!: ");
+	scanf("%12s", nev);
+	printf("Udvozollek %s!\n", nev);
+	return 0;
+}
+
