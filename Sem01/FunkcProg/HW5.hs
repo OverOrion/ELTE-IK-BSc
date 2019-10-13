ef4d4425def607a4cbf39a78765d094908c3d33a
OverOrion <szilard.parrag@gmail.com>
2019-10-13 16:36:52 +0200

    WIP
    Made some progress, only the last exercise is not ready yet.
    ~To understand recursion you must first understand recursion~

---
+++ Sem01/FunkcProg/HW5.hs
@@ -0,0 +1,49 @@
+module HW5 where 
+import Data.List
+
+--list
+--f1
+compress :: String -> [(Int, Char)]
+compress s = [(length e, head e) | e <- group s]
+
+--f2
+decompress :: [(Int, Char)] -> String
+decompress x = concat [replicate n c | (n, c) <- x]
+
+
+--recursion
+--f3
+fib 0 = 0
+fib 1 = 1 
+fib 2 = 1 
+fib n = fib (n-1) + fib (n-2)
+
+--f4
+or' :: [Bool] -> Bool
+or' [] = False
+or' (x:xs) = x || or' xs
+
+--f5
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' x (e:es) = x == e || elem' x es
+
+--f6
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+replicate' 1 x = [x]
+replicate' n x = x : replicate' (n-1) x
+
+--f7
+eqList :: Eq a => [a] -> [a] -> Bool
+eqList [] [] = True
+eqList [x] [y] = x == y
+eqList (x:xs) (y:ys) = x == y && eqList xs ys
+
+--f8
+dropEvery3 :: [a] -> [a]
+dropEvery3 (x:y:z) = x : y : dropEvery3 z
+
+--f9
+mapLength :: [[a]] -> [Int] 
+mapLength [x:xs] = length x : mapLength xs
