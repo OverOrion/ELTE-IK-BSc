a27574ba20afe594c0b40bc1dd607d3a21b40341
OverOrion <szilard.parrag@gmail.com>
2019-12-09 13:21:09 +0100

    AoC Day-2 in PWSH

---
+++ Sem01/SzamRend/PowerShell_ZH/AoC_2019/Day1.ps1
@@ -0,0 +1,8 @@
+﻿### Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+
+$sum=0
+foreach ($line in Get-Content .\Day1_input.txt)
+{
+    $sum+=[Math]::Floor((([int]$line/3)))-2
+}
+$sum
---
+++ Sem01/SzamRend/PowerShell_ZH/AoC_2019/Day1_2.ps1
@@ -0,0 +1,15 @@
+﻿### Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+
+$sum=0
+foreach ($line in Get-Content .\Day1_input.txt)
+{
+    $moduleMass=[Math]::Floor((([int]$line/3)))-2
+    $sum+=$moduleMass
+    $moduleMass = [Math]::Floor((([int]$moduleMass/3)))-2
+    while ($moduleMass -gt 0) 
+    {
+        $sum+=$moduleMass
+        $moduleMass = [Math]::Floor((([int]$moduleMass/3)))-2
+    }
+}
+$sum
